367646dc50494175e9ef25427b0aea5354ffd6fb
Juan Carrillo <juan@juanca.org>
2022-05-19 14:28:33 -0500

    feat(app): add displayPhotos() function to display photos from the Unsplash API. Capped to 10 photos per fetch

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+// Get elements from index.html
+const IMAGE_CONTAINER = document.getElementById('image-container');
+const LOADER = document.getElementById('loader');
+
+// The photos array that will contain the fetched photos
+// from the Unsplash API.
+let photosArray = [];
+
 // Unsplash API
-const COUNT = 5;
+const COUNT = 10;
 const API_KEY = 'demo';
 const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${COUNT}`;
 
-// Fetch photos from Unsplash API
+// Display photos in the DOM fetched from the Unsplash API.
+function displayPhotos() {
+	// Run function for each object in photosArray.
+	photosArray.forEach((photo) => {
+		// Create an <a> element to link to Unsplash.
+		const ITEM = document.createElement('a');
+		ITEM.setAttribute('href', photo.links.html);
+		ITEM.setAttribute('target', '_blank');
+
+		// Create an <img> element to set the photo in the page.
+		const IMAGE = document.createElement('img');
+		IMAGE.setAttribute('src', photo.urls.regular);
+		// Most photos have an 'alt_description=null', so we are going to use 'description' to set the alt description for our feed.
+		IMAGE.setAttribute('alt', photo.description);
+		IMAGE.setAttribute('title', photo.description);
+
+		// Put <img> inside the <a> element, then put both inside the IMAGE_CONTAINER.
+		ITEM.appendChild(IMAGE);
+		IMAGE_CONTAINER.appendChild(ITEM);
+	});
+}
+
+// Fetch photos from Unsplash API.
 async function getPhotos() {
 	const RESPONSE = await fetch(API_URL);
-	const DATA = await RESPONSE.json();
-	console.log(DATA);
+	photosArray = await RESPONSE.json();
+	console.log(photosArray);
+	displayPhotos();
 }
 
 getPhotos();
